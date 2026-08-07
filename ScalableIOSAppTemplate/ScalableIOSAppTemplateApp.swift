7f5be6dcba28cece262d971e7347586791ca7871
Nikola Popovic <nikolapo@mts-si.rs>
2026-08-07 13:50:26 +0200

    Initial Commit

---
+++ ScalableIOSAppTemplate/ScalableIOSAppTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScalableIOSAppTemplateApp.swift
+//  ScalableIOSAppTemplate
+//
+//  Created by Nikola Popovic on 7. 8. 2026..
+//
+
+import SwiftUI
+
+@main
+struct ScalableIOSAppTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
